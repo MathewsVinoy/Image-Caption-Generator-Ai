d86f83fb9fed5721601c6d62520556f860b649de
MathewsVinoy <mathewsvinoy@gmail.com>
2025-04-20 11:14:52 +0530

    data sets load in function buildin custimization

--- model.py
+++ model.py
@@ -1,5 +1,19 @@
-import touch
-from torch import nn
+import torch 
+from torch import nn 
 
-class Model(nn.Model):
-    pass
+class Model(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super(Model, self).__init__()
+        self.resnet = torchvision.models.resnet18(pretrained=True)
+        self.resnet.fc = nn.Identity() 
+        self.lstm = nn.LSTM(input_size=input_shape, hidden_size=hidden_units, batch_first=True)
+        self.fc = nn.Linear(hidden_units, output_shape)
+
+    def forward(self, x: torch.Tensor):
+        batch_size = x.size(0)
+        features = self.resnet(x)  
+        features = features.view(batch_size, 1, -1)
+        lstm_out, (h_n, c_n) = self.lstm(features)
+        lstm_out_last = lstm_out[:, -1, :]  
+        output = self.fc(lstm_out_last)
+        return output
