d99a1da3ee4a0b7461411e59e4b64b69043da9cf
MathewsVinoy <mathewsvinoy@gmail.com>
2025-04-26 10:06:26 +0530

    update

---
+++ endecode.py
@@ -0,0 +1,15 @@
+words_to_num = {char: idx for idx, char in enumerate(set(tokens))}
+num_to_words = {idx: char for idx, char in enumerate(set(tokens))}
+
+encode = lambda s: [words_to_num[c] for c in word_tokenize(s)]
+def encode2(l):
+
+    l=str(l)
+    value = [words_to_num[c] for c in word_tokenize(l)]
+    while len(value) <82:
+        value.append(0)
+    return torch.tensor(value, dtype=torch.float32)
+
+    
+def decoder(l):
+    return ' '.join([num_to_words.get(i,'<UNK>') for i in l])
