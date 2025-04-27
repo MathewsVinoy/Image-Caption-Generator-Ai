df6351dadf9edb9e7d8a3d93795fdaaf29452473
MathewsVinoy <mathewsvinoy@gmail.com>
2025-04-28 00:08:37 +0530

    making classs

--- endecode.py
+++ endecode.py
@@ -1,17 +1,18 @@
 import torch
 
-words_to_num = {char: idx for idx, char in enumerate(set(tokens))}
-num_to_words = {idx: char for idx, char in enumerate(set(tokens))}
+class DataConverter:
+    words_to_num = {char: idx for idx, char in enumerate(set(tokens))}
+    num_to_words = {idx: char for idx, char in enumerate(set(tokens))}
 
-encode = lambda s: [words_to_num[c] for c in word_tokenize(s)]
-def encode2(l):
+    encode = lambda s: [words_to_num[c] for c in word_tokenize(s)]
+    def encode2(l):
 
-    l=str(l)
-    value = [words_to_num[c] for c in word_tokenize(l)]
-    while len(value) <82:
-        value.append(0)
-    return torch.tensor(value, dtype=torch.float32)
+        l=str(l)
+        value = [words_to_num[c] for c in word_tokenize(l)]
+        while len(value) <82:
+            value.append(0)
+        return torch.tensor(value, dtype=torch.float32)
 
-    
-def decoder(l):
-    return ' '.join([num_to_words.get(i,'<UNK>') for i in l])
+        
+    def decoder(l):
+        return ' '.join([num_to_words.get(i,'<UNK>') for i in l])
