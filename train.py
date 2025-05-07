5ef7eb6c637758af089a018c91818d6357ddb2e3
MathewsVinoy <mathewsvinoy@gmail.com>
2025-05-08 05:12:54 +0530

    importations

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
 import torch
+from torch import nn
 from model import Model
 from dataSets import DatasetsCustom
+
+EPOCHS = 10
+loss_fn = nn.CrossEntropyLoss()
+model = Model()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
