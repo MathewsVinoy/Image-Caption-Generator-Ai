4da81da4ebab96c634f05fe4cdf1d4c98c36d810
MathewsVinoy <mathewsvinoy@gmail.com>
2025-05-09 23:17:41 +0530

    proper importation

--- train.py
+++ train.py
@@ -5,5 +5,5 @@ from dataSets import DatasetsCustom
 
 EPOCHS = 10
 loss_fn = nn.CrossEntropyLoss()
-model = Model()
+model = Model(input_shape=512,output_shape=82,hidden_units=10).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
